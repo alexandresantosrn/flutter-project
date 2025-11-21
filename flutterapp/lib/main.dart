cf2d68550807af30259eacc8194f43bd8e985c82
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2025-11-21 12:51:37 -0300

    develop: implementing layout.

--- flutterapp/lib/main.dart
+++ flutterapp/lib/main.dart
@@ -1,49 +1,87 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutterapp',
-      home: HomePage(),
+    return const CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeTabs(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
+class HomeTabs extends StatefulWidget {
+  const HomeTabs({super.key});
+
   @override
-  _HomePageState createState() => _HomePageState();
+  State<HomeTabs> createState() => _HomeTabsState();
 }
 
-class _HomePageState extends State<HomePage> {
-  String message = '';
+class _HomeTabsState extends State<HomeTabs> {
+  final CupertinoTabController _controller =
+      CupertinoTabController(initialIndex: 0);
+
+  static const List<String> _titles = [
+    'Configurações',
+    'Ação',
+    'Estatísticas',
+    'Histórico',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Exemplo')),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  message = 'Hello World!';
-                });
-              },
-              child: Text('Iniciar'),
-            ),
-            SizedBox(height: 16),
-            Text(
-              message,
-              style: TextStyle(fontSize: 24),
-            ),
-          ],
-        ),
+    return CupertinoTabScaffold(
+      controller: _controller,
+      tabBar: CupertinoTabBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.settings),
+            label: 'Configurações',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.bolt),
+            label: 'Ação',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.chart_bar),
+            label: 'Estatísticas',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.time),
+            label: 'Histórico',
+          ),
+        ],
       ),
+      tabBuilder: (context, index) {
+        return CupertinoTabView(
+          builder: (context) {
+            return CupertinoPageScaffold(
+              navigationBar: CupertinoNavigationBar(
+                middle: Text(_titles[index]),
+              ),
+              child: SafeArea(
+                child: Center(
+                  child: Text(
+                    _titles[index],
+                    style: const TextStyle(
+                        fontSize: 28, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
